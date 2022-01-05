d2ad45fbd97ffddb3792bac3de920560beef7e06
potados99 <potados99@gmail.com>
2022-01-05 23:16:40 +0900

    feat: User 엔티티 대략 작성

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,25 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
 
+/**
+ * 사용자!
+ */
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn({comment: '식별자.'})
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column({comment: '가입한 이메일.'})
+  email: string;
 
-    @Column()
-    firstName: string;
+  @Column({comment: '사용자 닉네임.'})
+  nickname: string;
 
-    @Column()
-    lastName: string;
+  @Column({comment: '로그인한 방법(카카오, 구글 등).'})
+  oauthProvider: string;
 
-    @Column()
-    age: number;
+  @Column({comment: 'OAuth로 로그인한 경우, provider가 제공한 식별자.'})
+  oauthId: string;
 
+  @Column({comment: '가입일.'})
+  createdAt: Date = new Date();
 }
