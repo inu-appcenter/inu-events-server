e532f4e1f8afec4fdc7732cc042d481b2ec47849
seokahi <skh9797@naver.com>
2022-01-06 03:57:27 +0900

    feat: jest 테스트 추가

---
+++ test/jest.test.ts
@@ -0,0 +1,12 @@
+describe("simple test", () => {
+  
+    it('1 + 1 = 2 ?', () => {
+      expect(1+1).toEqual(1);
+    });
+    //failed
+    it('2 + 3 = 5 ?', () => {
+      expect(2+3).toBe(5);
+    });
+    //passed
+  })
+  
