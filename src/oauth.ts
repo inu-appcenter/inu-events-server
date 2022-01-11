83cd5563852c68ab74d22e10c0cf4691715ec50d
potados99 <potados99@gmail.com>
2022-01-11 22:05:50 +0900

    feat: 구글 oauth 정보 가져오는 getGoogleOAuthInfo 함수 추가

---
+++ src/oauth.ts
@@ -0,0 +1,16 @@
+import {OAuth2Client} from 'google-auth-library';
+
+/**
+ * 구글에게 이 사용자의 이메일과 식별자(OpenID Connect에서는 subject라는 단어를 사용하나 여기에서는 oauthId라고 부를게여)를 물어봅니다.
+ * OpenID Connect는 https://www.ibm.com/docs/ko/sva/9.0.7?topic=concepts-openid-connect 참조!
+ *
+ * @param accessToken 클라이언트가 들고 온 액세스 토큰.
+ */
+export async function getGoogleOAuthInfo(accessToken: string) {
+  const info = await new OAuth2Client().getTokenInfo(accessToken);
+
+  return {
+    email: info.email,
+    oauthId: info.sub,
+  };
+}
