139d7731b64e664b7e9c58a2d9feaa1d7dc46022
potados99 <potados99@gmail.com>
2022-01-11 22:51:40 +0900

    chore: koa 라우터 쓰는 기초 예제(?) 추가

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import {createConnection} from 'typeorm';
+import * as Koa from 'koa';
+import * as Router from '@koa/router';
 
 async function run() {
-  const connection = await createConnection();
+  // const connection = await createConnection();
+  // await connection.synchronize(true);
 
-  await connection.synchronize(true);
+  const app = new Koa();
+  const router = new Router();
 
-  console.log('ㅎㅎ');
+  router.get('/hello', async (ctx) => {
+    console.log('요청옴!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+    ctx.body = 'ㅎㅇㅎㅇ';
+  });
+
+  app
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .listen(process.env.PORT || 3000);
+
+  console.log('서버시작!!!!!!!!!!');
 }
 
 run().then().catch((e) => console.log(e));
