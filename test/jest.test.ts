01964ea0251454eaebadc1ea2fbffac6fd7eb2c7
algosketch <sjy_9484@naver.com>
2022-01-17 03:57:16 +0900

    add : test example

--- test/jest.test.ts
+++ test/jest.test.ts
@@ -1,7 +1,7 @@
 describe("simple test", () => {
   
     it('1 + 1 = 2 ?', () => {
-      expect(1+1).toEqual(1);
+      expect(1+1).not.toEqual(1);
     });
     //failed
     it('2 + 3 = 5 ?', () => {
