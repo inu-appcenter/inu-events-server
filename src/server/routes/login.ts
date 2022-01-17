587e3fcc097e05bf4f186ab47a470cab18d470e3
potados99 <potados99@gmail.com>
2022-01-18 01:51:01 +0900

    feat: 구굴 로그인 작동히는거가틈

--- src/server/routes/login.ts
+++ src/server/routes/login.ts
@@ -1,19 +1,30 @@
-import express from 'express';
+import {defineSchema} from '../libs/schema';
+import {z} from 'zod';
+import {defineRoute} from '../libs/route';
 import {getGoogleOAuthInfo} from '../../oauth';
+import Unauthorized from '../../common/errors/http/Unauthorized';
 
-const router = express.Router();
-
-router.post('/login', async (req, res) => {
-  const token = req.body.accessToken;
-
-  if (token == null) {
-    res.sendStatus(400);
-    return;
+const schema = defineSchema({
+  body: {
+    accessToken: z.string(),
   }
-
-  const userInfo = getGoogleOAuthInfo(token);
-
-  // TODO
 });
 
-export default router;
+const WrongAuth = Unauthorized.of(
+  'wrong_auth',
+  '인증 이상함!!!!!!!!!!'
+);
+
+export default defineRoute('post', '/login', schema, async (req, res) => {
+  const {accessToken} = req.body;
+
+  try {
+    const userInfo = await getGoogleOAuthInfo(accessToken);
+
+    return res.send(`당신 이메일은 ${userInfo.email}`);
+  } catch (e) {
+    throw WrongAuth();
+  }
+});
+
+
