47620cd033b77cea49317919534aa817d56c57b6
potados99 <potados99@gmail.com>
2022-01-17 23:25:07 +0900

    chore: koa 제거 & express 설치

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,10 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import * as Koa from 'koa';
-
-import helloRoutes from './routes/hello';
-import byeRoutes from './routes/bye';
-// 여기에 추가
 
 async function run() {
   // const connection = await createConnection();
   // await connection.synchronize(true);
 
-  const app = new Koa();
-
-  app
-    .use(helloRoutes.routes()).use(helloRoutes.allowedMethods())
-    .use(byeRoutes.routes()).use(byeRoutes.allowedMethods())
-    // 여기에 추가
-    .listen(process.env.PORT || 3000);
 
   console.log('서버시작!!!!!!!!!!');
 }
