24e210276d43974bc331b440215d3feea91d0cd1
potados99 <potados99@gmail.com>
2022-01-17 23:40:11 +0900

    chore: express 틀 잡는중,,,

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
 async function run() {
   // const connection = await createConnection();
   // await connection.synchronize(true);
 
+  const app = express();
+
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded({extended: true}));
+
+  // app.use(auth());
+
+  app.get('/', (req, res, next) => {
+   // 본 로직
+    res.send();
+  });
+
+
+  app.listen(process.env.PORT || 3000);
 
   console.log('서버시작!!!!!!!!!!');
 }
