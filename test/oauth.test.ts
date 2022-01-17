d782d9be4e380b571095fdf680c833a56ece78b2
potados99 <potados99@gmail.com>
2022-01-18 00:00:34 +0900

    chore(test): access token으로 이메일 빼오는 테스트 성공 예시 추가

--- test/oauth.test.ts
+++ test/oauth.test.ts
@@ -1,5 +1,4 @@
 import {getGoogleOAuthInfo} from '../src/oauth';
-import Any = jasmine.Any;
 
 describe('구글 OAuth 라이브러리', () => {
   it('터뜨리기', async () => {
@@ -13,5 +12,11 @@ describe('구글 OAuth 라이브러리', () => {
         console.error('이건모임??????????');
       }
     }
+  });
+
+  it('액세스 토큰이 멀쩡하면 작동해야 함!', async () => {
+    const info = await getGoogleOAuthInfo('ya29.A0ARrdaM-95ti0QCOehpmI6z6lu3mvs1EvT_I6l_NvC789ZgG7gTh48uktOuBslmSsMZK1jMSh2Ob6nlZGmtDbNUJhCksBanOnVYevdkIo52YK7xktsgte8ikgb-ujFbdbwaXZFug_aeA_ImhBXDAp043Dteay');
+
+    console.log(info);
   });
 });
