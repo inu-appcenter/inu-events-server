bb20882804003bc3d35adb29facdcde2ac4b5f2f
potados99 <potados99@gmail.com>
2022-01-21 23:35:38 +0900

    fix: ormconfig 파일 이동:
    최상단(src 밖)에 두지 않고, createConnection의 인자로 직접 넘겨줌.

--- src/infrastructure/db.ts
+++ src/infrastructure/db.ts
@@ -1,7 +1,9 @@
 import {createConnection} from "typeorm";
+import ormconfig from '../ormconfig';
 
 export async function startTypeORM() {
-  const connection = await createConnection();
+  const connection = await createConnection(ormconfig);
   await connection.synchronize(true);
+
   console.log('db 연결 성공');
 }
