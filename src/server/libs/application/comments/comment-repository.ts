fbeb29e85a62287fe44c8a8f547d357ec02020f7
seokahi <skh9797@naver.com>
2022-01-25 03:36:44 +0900

    refactor: comment-repository경로변경

---
+++ src/server/libs/application/comments/comment-repository.ts
@@ -0,0 +1,46 @@
+import { EntityRepository, Repository } from 'typeorm';
+import User from '../../../../entity/User';
+import Event from '../../../../entity/Event';
+import Comment from '../../../../entity/Comment';
+
+@EntityRepository(Comment)
+class CommentRepository extends Repository<Comment> {
+    // 일단 accessToken으로 email 받아옴
+    async returnUser(email: string): Promise<User> {
+        const user = await User.createQueryBuilder('user').where('user.email = :email', { email }).getOne();
+        console.log(user);
+        return user;
+    }
+
+    async returnEvent (id: string): Promise<Event> {
+        const event = await Event.createQueryBuilder('event').where('event.id = :id', { id }).getOne();
+        console.log(event);
+        return event;
+    }
+    
+    async createComment(user: User,event:Event, content: string) {
+
+        const comment = await Comment.createQueryBuilder().
+                                    insert().into(Comment).
+                                    values({ user, event,content}).execute();
+        return comment.identifiers[0].id;
+
+    }
+
+    async deleteComment(commentId:string): Promise<string> {
+        const event = await Comment.createQueryBuilder().
+                                delete().from(Comment).
+                                where("id = :id", {id:commentId}).execute();
+        return event.raw;
+    }
+
+    async patchComment(commentId:string, req_query:Object): Promise<string> {
+        const user = await Comment.createQueryBuilder().
+                                update(Comment).
+                                set(req_query).
+                                where("id = :id", {id:commentId}).execute();
+        return user.raw;
+    }
+}
+
+export default CommentRepository;
