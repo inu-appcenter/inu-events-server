9b4eac4132fc90d43d0084c121bd67be45dbd2f4
potados99 <potados99@gmail.com>
2022-01-25 22:01:06 +0900

    chore: User 생성 active record pattern 예시 주석 추가

--- src/server/routes/users/login.ts
+++ src/server/routes/users/login.ts
@@ -4,7 +4,8 @@ import UserRepository from '../../libs/application/user/user-repository';
 import {defineRoute} from '../../libs/route';
 import {getGoogleOAuthInfo} from '../../../oauth';
 import Unauthorized from '../../../common/errors/http/Unauthorized';
-import { getCustomRepository } from 'typeorm';
+import {getCustomRepository} from 'typeorm';
+import User from '../../../entity/User';
 
 
 const schema = defineSchema({
@@ -25,12 +26,18 @@ export default defineRoute('post', '/login', schema, async (req, res) => {
     const userInfo = await getGoogleOAuthInfo(accessToken);
     const existEmail = await getCustomRepository(UserRepository).checkEmail(userInfo.email);
     if (existEmail) {
-      return res.json({"email": `${userInfo.email}`, "oauthId":  `${userInfo.oauthId}`, "msg" : "이미 가입된 사용자입니다."});
+      return res.json({'email': `${userInfo.email}`, 'oauthId': `${userInfo.oauthId}`, 'msg': '이미 가입된 사용자입니다.'});
+    } else {
+      // await User.create({
+      //   email: userInfo.email,
+      //   nickname: 'test',
+      //   oauthProvider: 'google',
+      //   oauthId: userInfo.oauthId
+      // }).save();
+      //
+      await getCustomRepository(UserRepository).createUser(userInfo.email, 'test', 'google', userInfo.oauthId);
     }
-    else {
-      await getCustomRepository(UserRepository).createUser(userInfo.email,"test","google",userInfo.oauthId);
-    }
-    return res.json({"email": `${userInfo.email}`, "oauthId":  `${userInfo.oauthId}`,  "msg" : "회원가입이 완료되었습니다."});
+    return res.json({'email': `${userInfo.email}`, 'oauthId': `${userInfo.oauthId}`, 'msg': '회원가입이 완료되었습니다.'});
 
   } catch (e) {
     throw WrongAuth();
