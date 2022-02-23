7c45050a837ffbad3eef05f8d82a38244bdac293
potados99 <potados99@gmail.com>
2022-02-24 00:27:38 +0900

    chore: 새 에러 NoSuchResource 추가

--- src/common/errors/general.ts
+++ src/common/errors/general.ts
@@ -1,5 +1,6 @@
 import InternalServerError from './http/InternalServerError';
 import Unauthorized from './http/Unauthorized';
+import NotFound from './http/NotFound';
 
 export const ThisWillNeverHappen = InternalServerError.of(
   'this_will_never_happen',
@@ -9,3 +10,5 @@ export const ThisWillNeverHappen = InternalServerError.of(
 export const NotLoggedIn = Unauthorized.of('not_logged_in', '로그인해주세요!');
 
 export const InvalidJwt = Unauthorized.of('invalid_jwt', '로그인해주세요!');
+
+export const NoSuchResource = NotFound.of('no_such_resource', '해당 리소스를 찾을 수 없습니다!');
