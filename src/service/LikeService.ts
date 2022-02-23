413ccb2c441034c11edd399186d58d130ebdbf19
potados99 <potados99@gmail.com>
2022-02-24 00:28:46 +0900

    feat: LikeService 초안

---
+++ src/service/LikeService.ts
@@ -0,0 +1,48 @@
+import User from '../entity/User';
+import Event from '../entity/Event';
+import EventLike from '../entity/EventLike';
+import {NoSuchResource} from '../common/errors/general';
+import assert from 'assert';
+import {log} from '../common/utils/log';
+
+class LikeService {
+  async performLike(userId: number, eventId: number) {
+    const theUser = await User.findOne(userId);
+    const theEvent = await Event.findOne(eventId);
+
+    assert(theUser, NoSuchResource());
+    assert(theEvent, NoSuchResource());
+
+    const existingLike = await EventLike.findOne({where: {user: theUser, event: theEvent}});
+
+    if (existingLike) {
+      log(`${theUser.toString()}는 이미 ${theEvent.toString()}를 like 합니다.`);
+      return;
+    }
+
+    await EventLike.create({user: theUser, event: theEvent}).save();
+
+    log(`${theUser.toString()}가 ${theEvent.toString()}를 좋아하는 EventLike를 생성했습니다.`);
+  }
+
+  async performUnlike(userId: number, eventId: number) {
+    const theUser = await User.findOne(userId);
+    const theEvent = await Event.findOne(eventId);
+
+    assert(theUser, NoSuchResource());
+    assert(theEvent, NoSuchResource());
+
+    const existingLike = await EventLike.findOne({where: {user: theUser, event: theEvent}});
+
+    if (existingLike == null) {
+      log(`${theUser.toString()}는 ${theEvent.toString()}를 like한 적이 없습니다!!`);
+      return;
+    }
+
+    await existingLike.remove();
+
+    log(`${theUser.toString()}는 이제 ${theEvent.toString()}를 좋아하지 않습니다.`);
+  }
+}
+
+export default new LikeService();
