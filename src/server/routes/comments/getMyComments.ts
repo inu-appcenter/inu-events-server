a5086bb8c873fee8fbe721aecb37d5c9d46fdc0c
potados99 <potados99@gmail.com>
2022-03-01 01:18:56 +0900

    fix: authorizer 설치

--- src/server/routes/comments/getMyComments.ts
+++ src/server/routes/comments/getMyComments.ts
@@ -2,6 +2,7 @@ import {defineSchema} from '../../libs/schema';
 import {defineRoute} from '../../libs/route';
 import CommentService from '../../../service/CommentService';
 import {CommentResponseScheme} from '../../../entity/schemes';
+import {authorizer} from '../../middleware/authorizer';
 
 const schema = defineSchema({
   summary: '내가 쓴 댓글을 다 가져옵니다.',
@@ -10,8 +11,10 @@ const schema = defineSchema({
   response: [CommentResponseScheme]
 });
 
-export default defineRoute('get', '/mycomments', schema, async (req, res) => {
+export default defineRoute('get', '/mycomments', schema, authorizer(), async (req, res) => {
   const userId = req.requireUserId();
+
   const commentInformation = await CommentService.getMyComments(userId);
+
   return res.json(await Promise.all(commentInformation.map(c => c.toResponse(userId))));
 });
--- src/server/routes/events/getEventsIveCommentedOn.ts
+++ src/server/routes/events/getEventsIveCommentedOn.ts
@@ -2,15 +2,16 @@ import {defineSchema} from '../../libs/schema';
 import {defineRoute} from '../../libs/route';
 import {EventResponseScheme} from '../../../entity/schemes';
 import EventService from '../../../service/EventService';
+import {authorizer} from '../../middleware/authorizer';
 
 const schema = defineSchema({
-  summary: '내가 쓴 댓글이 딸린 이벤트를 다 가져옵니다.',
+  summary: '내가 댓글 단 이벤트를 다 가져옵니다.',
   description: '이벤트 id 역순으로 가져옵니다.',
 
   response: [EventResponseScheme]
 });
 
-export default defineRoute('get', '/events-ive-commented', schema, async (req, res) => {
+export default defineRoute('get', '/events-ive-commented', schema, authorizer(), async (req, res) => {
   const userId = req.requireUserId();
 
   const events = await EventService.getEventsIveCommentedOn(userId);
