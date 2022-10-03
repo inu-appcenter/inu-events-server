1798487fa09a04dc07846e1d7d1a3de8c68d5dbe
potados99 <potados99@gmail.com>
2022-10-03 21:31:08 +0900

    refactor: 구글과 애플 로그인 엔드포인트 분리

---
+++ src/server/routes/login/googleOAuthLogin.ts
@@ -0,0 +1,37 @@
+import {defineSchema} from '../../libs/schema';
+import {z} from 'zod';
+import {defineRoute} from '../../libs/route';
+import LoginService from '../../../service/LoginService';
+import config from '../../../config';
+
+const schema = defineSchema({
+  summary: 'Google OAuth 로그인',
+  description: '내 정보를 가져옵니다.',
+
+  body: {
+    accessToken: z.string(),
+  },
+
+  response: {
+    jwt: z.string(),
+    userId: z.number(),
+    rememberMeToken: z.string()
+  }
+});
+
+export default defineRoute('post', '/login/oauth/google', schema, async (req, res) => {
+  const {accessToken} = req.body;
+
+  const {user, jwt, rememberMeToken} = await LoginService.googleOAuthLogin(accessToken);
+
+  return res
+    .header('token', jwt)
+    .cookie(config.server.jwt.cookieName, jwt, config.server.jwt.cookieOptions)
+    .json({
+      jwt,
+
+      userId: user.id,
+      rememberMeToken,
+    });
+});
+
