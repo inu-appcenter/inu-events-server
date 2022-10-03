6771f9d5b579bd2800dd1ce36ae0504240ea7cab
potados99 <potados99@gmail.com>
2022-10-03 21:31:24 +0900

    refactor: 안 쓰는 서비스 제거

--- src/server/routes/login/appleOauthLogin.ts
+++ src/server/routes/login/appleOauthLogin.ts
@@ -1,37 +1,37 @@
 import {defineSchema} from '../../libs/schema';
 import {z} from 'zod';
 import {defineRoute} from '../../libs/route';
-import AppleLoginService from '../../../service/AppleLoginService';
+import LoginService from '../../../service/LoginService';
 import config from '../../../config';
 
 const schema = defineSchema({
-    summary: 'OAuth 로그인',
-    description: '내 정보를 가져옵니다.',
+  summary: 'Apple OAuth 로그인 (현재는 iOS 전용)',
+  description: '내 정보를 가져옵니다.',
 
-    body: {
-        accessToken: z.string(),
-    },
+  body: {
+    accessToken: z.string(),
+  },
 
-    response: {
-        jwt: z.string(),
-        userId: z.number(),
-        rememberMeToken: z.string()
-    }
+  response: {
+    jwt: z.string(),
+    userId: z.number(),
+    rememberMeToken: z.string()
+  }
 });
 
-export default defineRoute('post', '/login/appleoauth', schema, async (req, res) => {
-    const {accessToken} = req.body;
+export default defineRoute('post', '/login/oauth/apple', schema, async (req, res) => {
+  const {accessToken} = req.body;
 
-    const {user, jwt, rememberMeToken} = await AppleLoginService.oauthLogin(accessToken);
+  const {user, jwt, rememberMeToken} = await LoginService.appleOAuthLogin(accessToken);
 
-    return res
-        .header('token', jwt)
-        .cookie(config.server.jwt.cookieName, jwt, config.server.jwt.cookieOptions)
-        .json({
-            jwt,
+  return res
+    .header('token', jwt)
+    .cookie(config.server.jwt.cookieName, jwt, config.server.jwt.cookieOptions)
+    .json({
+      jwt,
 
-            userId: user.id,
-            rememberMeToken,
-        });
+      userId: user.id,
+      rememberMeToken,
+    });
 });
 
