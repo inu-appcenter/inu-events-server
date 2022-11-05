02b1a57f9e8349727296cf4a4d6c6ecb75801659
seohyun-kim <selenium34@inu.ac.kr>
2022-11-05 16:58:18 +0900

    fix : requireEnv에서 환경변수 못찾는 것 같으니 일단 직접 줘보자.

--- src/config.ts
+++ src/config.ts
@@ -24,19 +24,26 @@ export default {
     },
     storage: {
       image: {
-        path: requireEnv('IMAGE_STORAGE_PATH'),
+        path: process.env.IMAGE_STORAGE_PATH || '/Users/',
       }
     },
   },
 
   external: {
-    appleSignIn: {
-      bundleID: requireEnv('BUNDLE_ID'), /*iOS 기기*/
-      serviceID: requireEnv('SERVICE_ID'), /*웹 앱(Android 포함)*/
-      teamID: requireEnv('TEAM_ID'),
-      keyIdentifier: requireEnv('KEY_IEDNTIFIER'),
-      privateKey: requireEnv('PRIVATE_KEY'),
+    appleSignIn: { // 환경변수를 못찾는 것 같으니 일단 이렇게로 다 바꿔보자
+      bundleID: process.env.BUNDLE_ID || 'com.INUAppCenter.Uniletter', /*iOS 기기*/
+      serviceID: process.env.SERVICE_ID || 'com.INUAppCenter.Uniletter.SignIn', /*웹 앱(Android 포함)*/
+      teamID: process.env.TEAM_ID || 'secret',
+      keyIdentifier: process.env.KEY_IEDNTIFIER || 'secret',
+      privateKey: process.env.PRIVATE_KEY || 'secret',
       redirectUri: 'https://uniletter.inuappcenter.kr/login/appleoauth/callback',
+
+      // bundleID: requireEnv('BUNDLE_ID'), /*iOS 기기*/
+      // serviceID: requireEnv('SERVICE_ID'), /*웹 앱(Android 포함)*/
+      // teamID: requireEnv('TEAM_ID'),
+      // keyIdentifier: requireEnv('KEY_IEDNTIFIER'),
+      // privateKey: requireEnv('PRIVATE_KEY'),
+      // redirectUri: 'https://uniletter.inuappcenter.kr/login/appleoauth/callback',
     }
   }
 };
