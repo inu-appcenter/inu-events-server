0b3e16d1a072922067a69a2901cbb40e474d86a7
seohyun-kim <selenium34@inu.ac.kr>
2022-11-13 17:22:34 +0900

    chore : 개발용 애플로그인 관련 환경변수 가져오는 부분 이제 안알려줌.

--- src/server/routes/dev/appleSignInEnvCheck.ts
+++ src/server/routes/dev/appleSignInEnvCheck.ts
@@ -4,10 +4,11 @@ import config from "../../../config";
 
 const schema = defineSchema({
     summary: '[테스트] 서버의 애플관련 환경변수를 가져옵니다.',
-    description: '개발용입니다!',
+    description: '개발용입니다! 테스트 끝났으니까 이제 안알려줌.',
 });
 
 export default defineRoute('get', '/apple-env', schema, async (req, res) => {
-    return res.json(config.external.appleSignIn);
+    return res.json("안알려줌");
+    // return res.json(config.external.appleSignIn);
 });
 
