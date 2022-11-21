15ad48bc25952e3da8a5769e4fc18f9d6e5ef887
seohyun-kim <selenium34@inu.ac.kr>
2022-11-22 02:02:21 +0900

    fix : params 가 아니라 query!!!! 이제 잘 됨 (바보바보바보바보)

--- src/server/routes/events/getEventsbyPage.ts
+++ src/server/routes/events/getEventsbyPage.ts
@@ -4,12 +4,13 @@ import EventService from '../../../service/EventService';
 
 import {EventPageResponseScheme, EventResponseScheme} from '../../../entity/schemes';
 import {stringAsInt} from "../../libs/zodTypes";
+import {log} from "../../../common/utils/log";
 
 const schema = defineSchema({
     summary: '행사를 페이지 별로 가져옵니다.',
     description: '한 번에 pageSize 개씩, id 역순으로 !',
 
-    params: {
+    query: {
         pageNum: stringAsInt,
         pageSize:stringAsInt
     },
@@ -21,6 +22,7 @@ export default defineRoute('get', '/events-by-page', schema, async (req, res) =>
     const {userId} = req;
     const {pageNum, pageSize} = req.params;
 
+    log(`Route: pageNum 값은 ${pageNum} 입니다./ pageSize는 ${pageSize} 입니다.`)
     const eventInformation = await EventService.getEventsbyPage(userId, pageNum, pageSize);
 
     return res.json(await Promise.all(eventInformation.map(e => e.toResponse(userId))));
