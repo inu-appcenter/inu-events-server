74304e472dadd778170935b65e1a2f8f40294780
seohyun-kim <selenium34@inu.ac.kr>
2022-11-23 19:02:15 +0900

    fix : response type에 event만 내리도록 수정

--- src/server/routes/events/getEventsbyPage.ts
+++ src/server/routes/events/getEventsbyPage.ts
@@ -1,35 +1,35 @@
 import {defineSchema} from '../../libs/schema';
 import {defineRoute} from '../../libs/route';
 import EventService from '../../../service/EventService';
-import {EventPageResponseScheme} from '../../../entity/schemes';
+import {EventResponseScheme} from '../../../entity/schemes';
 import {stringAsInt} from "../../libs/zodTypes";
+import {log} from "../../../common/utils/log";
 
 const schema = defineSchema({
     summary: '행사를 페이지 별로 가져옵니다.',
     description: '- pageNum 페이지 부터, 한 번에 pageSize 개씩, id 역순으로 ! \n' +
         ' - pageNum은 0부터 시작입니다. \n' +
-        '- maxPage는 올림한 값입니다. 하나라도 비어있으면 오류남.\n' +
+        '- query 하나라도 비어있으면 400 오류 납니다. (optional로 할까요?) \n' +
         ' - pageSize= 0이면 전체 이벤트 내려줌',
 
     query: {
-        pageNum: stringAsInt,
-        pageSize:stringAsInt
+        pageNum: stringAsInt.optional(), //.optional() 로 줄까말까
+        pageSize:stringAsInt.optional()
     },
 
-    response: [EventPageResponseScheme]
+    response: [EventResponseScheme]
 });
 
 export default defineRoute('get', '/events-by-page', schema, async (req, res) => {
     const {userId} = req;
     const {pageNum, pageSize} = req.query;
-
-    const eventInformation = await EventService.getEventsbyPage(userId, pageNum, pageSize);
-    const totalEvent = await EventService.getTotalEvent();
-
-    const eventPageInformation = {  maxPage: Math.floor(totalEvent / pageSize) ,
-        totalEvent: totalEvent,
-        event: eventInformation,
+    let eventInformation;
+    if(pageNum == null  || pageSize == null){ // 하나라도 비어있으면
+        log(`pageNum: ${pageNum}, pageSize: ${pageSize} 하나라도 비어있으면 전체 결과 출력합니다!`);
+        eventInformation = await EventService.getEvents(userId); // 전체 결과
+    }else{
+        eventInformation = await EventService.getEventsbyPage(userId, pageNum, pageSize); // 페이징 적용
     }
 
-    return res.json(eventPageInformation);
+    return res.json(await Promise.all(eventInformation.map(e => e.toResponse(userId))));
 });
