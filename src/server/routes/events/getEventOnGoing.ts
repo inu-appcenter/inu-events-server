4dac34171c95b9058de8be62c7d91e6c60baede2
seohyun-kim <selenium34@inu.ac.kr>
2022-11-23 19:23:07 +0900

    fix : events-ongoing 에서도 res응답 형태 이벤트만 내려주고, 페이징 적용 optional로 유지

--- src/server/routes/events/getEventOnGoing.ts
+++ src/server/routes/events/getEventOnGoing.ts
@@ -1,7 +1,7 @@
 import {defineSchema} from '../../libs/schema';
 import {defineRoute} from '../../libs/route';
 import EventService from '../../../service/EventService';
-import {EventPageResponseScheme, EventResponseScheme} from '../../../entity/schemes';
+import {EventResponseScheme} from '../../../entity/schemes';
 import {stringAsInt} from "../../libs/zodTypes";
 
 const schema = defineSchema({
@@ -9,15 +9,15 @@ const schema = defineSchema({
     description: '- 마감인 행사는 보여주지 않고, 진행 중인 행사만 내려줍니다. \n' +
         '- pageNum 페이지 부터, 한 번에 pageSize 개씩, id 역순으로 ! \n' +
         ' - pageNum은 0부터 시작입니다. \n' +
-        '- maxPage는 올림한 값입니다. 하나라도 비어있으면 오류남.\n' +
+        '-  - query 하나라도 비어있으면 전체 결과를 출력합니다. (이전 버전 호환용)\n' +
         ' - pageSize= 0이면 전체 이벤트 내려줌',
 
     query: {
-        pageNum: stringAsInt,
-        pageSize:stringAsInt
+        pageNum: stringAsInt.optional(),
+        pageSize:stringAsInt.optional()
     },
 
-    response: [EventPageResponseScheme]
+    response: [EventResponseScheme]
 });
 
 export default defineRoute('get', '/events-ongoing', schema, async (req, res) => {
@@ -25,12 +25,6 @@ export default defineRoute('get', '/events-ongoing', schema, async (req, res) =>
     const {pageNum, pageSize} = req.query;
 
     const eventInformation = await EventService.getEventsOnGoing(userId, pageNum, pageSize);
-    const totalEvent = await EventService.getOngoingTotalEvent();
 
-    const eventPageInformation = {  maxPage: Math.floor(totalEvent / pageSize) ,
-        totalEvent: totalEvent,
-        event: eventInformation,
-    }
-
-    return res.json(eventPageInformation);
+    return res.json(await Promise.all(eventInformation.map(e => e.toResponse(userId))));
 });
