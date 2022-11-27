0c09f3ca57264821e0ebc197fc10193cf55bcea7
potados99 <potados99@gmail.com>
2022-11-27 17:51:21 +0900

    chore: 카테고리 변경 내용을 문서에도 반영!

--- src/server/routes/events/getCategorybyFiltering.ts
+++ src/server/routes/events/getCategorybyFiltering.ts
@@ -7,7 +7,7 @@ import {EventResponseScheme} from '../../../entity/schemes';
 const schema = defineSchema({
   tags: ['히히'],
   summary: '카테고리 별로 필터링해서 가져옵니다',
-  description: ' - categoryId : `선택없음:0` `동아리/소모임:1` `학생회:2` `간식나눔:3` `대회/공모전:4` `스터디:5` `구인:6` `기타:7`\n'+
+  description: ' - categoryId : `모두선택:0` `동아리/소모임:1` `학생회:2` `간식나눔:3` `대회/공모전:4` `스터디:5` `구인:6` `기타:7` `교육/강연:8` \n'+
   ` - eventStatus : true 일 경우 해당 카테고리 중애서 진행 중인 이벤트만 가져옴, false 일 경우 진행 중인 이벤트와 상관없이 해당 카테고리 이벤트 다 가져옴\n`+
   ` - 페이징 적용(/get events 참고)`,
 
