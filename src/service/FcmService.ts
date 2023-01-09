4d2a89f449307d9b923c8b5a90bb030b6d6a99cf
seohyun-kim <selenium34@inu.ac.kr>
2023-01-09 15:29:28 +0900

    feat : FCM service send 에서 eventID 추가

--- src/service/FcmService.ts
+++ src/service/FcmService.ts
@@ -4,19 +4,21 @@ import {sendFcm} from '../infrastructure/fcm';
 import UserService from './UserService';
 
 class FcmService {
-  async send(user: User, title: string, body: string) {
+  async send(user: User,  title: string, body: string, eventId?:string|undefined) {
     if (user.fcmToken == null) {
       log(`${user.toString()}에게 알림을 쏘고 싶은데, fcmToken이 없어서 못보냄~`);
       return;
     }
 
-    await this.sendInternal(user.fcmToken, title, body);
+    await this.sendInternal(user.fcmToken, title, body, eventId);
+
+
   }
 
-  private async sendInternal(fcmToken: string, title: string, body: string) {
-    log(`[${fcmToken}] 여기로 알림 빵야 가즈ㅏㅏ! 제목은 [${title}], 내용은 [${body}]`);
 
-    await sendFcm(fcmToken, title, body);
+  private async sendInternal(fcmToken: string, title: string, body: string, eventId?:string|undefined) {
+    log(`[${fcmToken}] 여기로 알림 빵야 가즈ㅏㅏ! 제목은 [${title}], 내용은 [${body}] 이벤트 아이디는 [${eventId}]`);
+    await sendFcm(fcmToken, title, body, eventId);
   }
 
   async setFcmToken(userId: number, fcmToken: string) {
